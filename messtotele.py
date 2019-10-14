c28f4047288be6218d326bdae7b956cf9ec37bee
Md Sarwar Jahan Sabit <ssarwarjahan@gmail.com>
2019-10-15 02:36:39 +0600

    Update pyMessToTele

--- messtotele.py
+++ messtotele.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
 import sys
 import os
 import time
@@ -11,11 +14,18 @@ from getpass import getpass
 import string
 from pygtail import Pygtail
 
+
+
+
 TOKEN = '' #Add Bot Token (Search 'Botfather', follow instruction, create bot and get token).
 CHAT_ID = '' #Add your Chat ID (goto 'https://api.telegram.org/bot<token>/getupdates' for Chat ID). 
 
 # Use '/setinline' and '/setinlinefeedback' one after another and follow their instructions to enable InlineQuery for 
 # your telegram bot.
+
+names = []
+
+
 
 #bot = telepot.Bot(TOKEN)
 t = ''
@@ -27,14 +37,24 @@ dirpath = 'D:\\image.jpg'
 
 
 
-def normalSend(msg, receiver_id):
+def normalSend(msg, title, receiver_id):
     #content_type, chat_type, chat_id = telepot.glance(msg)
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                   [InlineKeyboardButton(text='Reply to ' + user, callback_data= receiver_id)],
+                   [InlineKeyboardButton(text= title, callback_data = receiver_id)],
                ])
 
     bot.sendMessage(CHAT_ID, msg, reply_markup=keyboard)
+    
+def normalSend0(msg, title, receiver_id):
+    #content_type, chat_type, chat_id = telepot.glance(msg)
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                   [InlineKeyboardButton(text= title, callback_data = receiver_id)],
+                   [InlineKeyboardButton(text= 'Last Messages', callback_data = 'read_msg' + receiver_id)]
+               ])
+
+    bot.sendMessage(CHAT_ID, msg, reply_markup=keyboard)    
 
 
 
@@ -46,9 +66,28 @@ def on_chat_message(msg):
     else:
         print('Error!')
     content_type, chat_type, chat_id = telepot.glance(msg)
-    print(msg)
+    #print(msg)
     print(content_type)
-    if content_type == 'text':
+    if msg['text'] == '/help':
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                   [InlineKeyboardButton(text='Show Last 20 Threads', callback_data='show_lt')],
+                   
+               ])
+
+        bot.sendMessage(chat_id, msg['text'], reply_markup=keyboard)
+    
+    elif '/searchUser' in msg['text']:
+        msg['text'] = msg['text'].replace('/searchUser ', '')
+        user = client.searchForUsers(msg['text'])[0]
+        print(user)
+        
+    elif '/search' in msg['text']:
+        msg['text'] = msg['text'].replace('/search ', '')
+        thread = client.searchForThreads(msg['text'])
+        print(thread)
+
+    
+    elif content_type == 'text':
         try:
             messageText = msg['text'].decode("utf-8")           
         except:
@@ -90,30 +129,35 @@ client.send(Message(text = msg), thread_id= t, thread_type=ThreadType.USER)
 
 
 class CustomClient(Client):
-    def onMessage(self, author_id, message_object, thread_id, thread_type, ts, metadata, msg, **kwargs):
-            global t 
-            t = author_id
-            thread = self.fetchThreadInfo(thread_id)[thread_id]
-            global user
-            user = thread.name
-            if author_id != self.uid:
-                if thread_type != ThreadType.GROUP:
-                    if message_object.text:
-                        print(str(thread_id) + " " + str(thread_type))
-                        #user = client.fetchUserInfo(user_id)[user_id]
-                        text = thread.name + ": " + message_object.text
-                        #on_chat_message(text)
-                        #bot.sendMessage(CHAT_ID, text)
-                        normalSend(text, t)
-                    elif len(message_object.attachments) > 0:
-                        for i in message_object.attachments:
-                            if isinstance(i, ImageAttachment):
-                                text = thread.name + ": " + i.large_preview_url
-                                normalSend(text, t)
-                    else:
-                        text = thread.name + " has sent you something! Check it on " + "https://www.facebook.com/messages/t/" + author_id
+    def onMessage(self, author_id, message_object, thread_id, thread_type, ts, metadata, msg, **kwargs):      
+        global t 
+        t = author_id
+        thread = self.fetchThreadInfo(thread_id)[thread_id]
+        global user
+        user = thread.name
+        if author_id != self.uid:
+            if thread_type != ThreadType.GROUP:
+                if message_object.text:
+                    print(str(thread_id) + " " + str(thread_type))
+                    #user = client.fetchUserInfo(user_id)[user_id]
+                    text = thread.name + ": " + message_object.text
+                    #on_chat_message(text)
+                    #bot.sendMessage(CHAT_ID, text)
+                    normalSend(text, '✏ Reply', t)
+                elif len(message_object.attachments) > 0:
+                    for i in message_object.attachments:
+                        if isinstance(i, ImageAttachment):
+                            text = thread.name + ": " + i.large_preview_url
+                            text = 'Reply to ' + thread.name
+                            normalSend(text, '✏ Reply', t)
+                else:
+                    text = thread.name + " has sent you something! Check it on " + "https://www.facebook.com/messages/t/" + author_id
+    
 
-def messageInput(Text, type):#######
+    
+    
+
+def messageInput(Text, type):
     global step, no1, q_data
     if step == 1:
         bot.sendMessage(CHAT_ID, Text)
@@ -139,9 +183,59 @@ def on_callback_query(msg):
     global q_data, step
     step = 1
     q_data = query_data
-    thread = client.fetchThreadInfo(query_data)[query_data]
-    messageText = 'Reply to ' + thread.name + ':'
-    messageInput(messageText, 'text')
+    if query_data == 'show_lt':
+        threads = client.fetchThreadList()
+        #print(threads)
+        #names = []
+        #q_data = []
+        for items in range(len(threads)):
+            #print(threads[items])
+            index1 = str(threads[items]).find(", name=") + 9
+            #print(index1)
+            index2 = str(threads[items]).find("', last_message_timestamp")
+            #print(index2)
+            name = str(threads[items])[index1:index2].decode('unicode-escape').encode('utf-8')
+            #names.append(name)
+            #names[items] = names[items].decode('unicode-escape').encode('utf-8')
+            
+            index1 = str(threads[items]).find("uid") + 5
+            #print(index1)
+            index2 = str(threads[items]).find("', type")
+            #print(index2)
+            q = str(threads[items])[index1:index2]
+            #q_data.append(q)
+            
+            normalSend0(name, '✏ Reply', q)
+    
+    elif 'read_msg' in query_data:
+        messages = client.fetchThreadMessages(thread_id=query_data.replace('read_msg', ''), limit=10)
+        messages.reverse()
+        #print(messages)
+        for items in range(len(messages)):
+            index1 = str(messages[items]).find("text='") + 16
+            index2 = str(messages[items]).find("', mentions")
+            mess = str(messages[items])[index1:index2].decode('unicode-escape').encode('utf-8')
+            
+            index1 = str(messages[items]).find("author='") + 8
+            index2 = str(messages[items]).find("', timestamp")
+            thread = str(messages[items])[index1:index2]
+            thread = client.fetchThreadInfo(thread)[thread]
+            thread = thread.name + ': '
+            try:
+                text = thread + mess
+            except:
+                text = thread.decode('unicode-escape').encode('utf-8') + mess
+            bot.sendMessage(CHAT_ID, text)
+        
+        thread = client.fetchThreadInfo(query_data.replace('read_msg', ''))[query_data.replace('read_msg', '')]    
+        normalSend('Reply to: ' + thread.name, '✏ Reply', query_data.replace('read_msg', ''))
+            
+        
+        #print(threads)    
+    else:    
+        thread = client.fetchThreadInfo(query_data)[query_data]
+        messageText = 'Reply to ' + thread.name + ':'
+        messageInput(messageText, 'text')
     
     
     
@@ -191,7 +285,7 @@ print 'Telegram bot has started Listening ...'
 print('Starting program...')
 client.listen()
 
-
+'''
 if __name__ == '__main__':
     try:
         main()
@@ -201,3 +295,4 @@ if __name__ == '__main__':
             sys.exit(0)
         except SystemExit:
             os._exit(0)
+'''
